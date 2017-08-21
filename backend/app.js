0f23da77e429de28d9b80c16e2a74135b815f572
mmoderwell <mmoderwell@gmail.com>
2017-08-20 23:12:27 -0500

    Styling changes

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,20 @@ const routes = require('./routes/routes');
 
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 routes(app);
 
 module.exports = app;
