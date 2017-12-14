dd62c87e227efb9bb776d4fb8bd26250a9189bd7
Matt Moderwell <mmoderwell@gmail.com>
2017-12-13 19:14:57 -0600

    Login to sage work

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,8 +1,10 @@
 const weather = require('../controllers/weather');
 const unsplash = require('../controllers/unsplash');
+const users = require('../controllers/users');
 
 module.exports = (app) => {
-    app.get('/api/weather/current', weather.current);
-    app.get('/api/weather/day', weather.day);
-    app.get('/api/unsplash/random', unsplash.random)
-};
+	app.get('/api/weather/current', weather.current);
+	app.get('/api/weather/day', weather.day);
+	app.get('/api/unsplash/random', unsplash.random);
+	app.post('/login', users.login);
+}
